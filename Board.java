ab7ce5d5ad1b8a4ad42be17d45fc5a0b83235089
ZhaoZJosh <zhao.zjosh@gmail.com>
2026-08-24 15:06:07 -0500

    Added pom.xml and basic board info

---
+++ Board.java
@@ -0,0 +1,63 @@
+public class Board {
+    //bitboard vars
+    private long wPawns;
+    private long wKnights;
+    private long wBishops;
+    private long wRooks;
+    private long wQueens;
+    private long wKing;
+    private long bPawns;
+    private long bKnights;
+    private long bBishops;
+    private long bRooks;
+    private long bQueens;
+    private long bKing;
+
+    public long getWPawns() {
+        return wPawns;
+    }
+
+    public long getWKnights() {
+        return wKnights;
+    }
+
+    public long getWBishops() {
+        return wBishops;
+    }
+
+    public long getWRooks() {
+        return wRooks;
+    }
+
+    public long getWQueens() {
+        return wQueens;
+    }
+
+    public long getWKing() {
+        return wKing;
+    }
+
+    public long getBPawns() {
+        return bPawns;
+    }
+
+    public long getBKnights() {
+        return bKnights;
+    }
+
+    public long getBBishops() {
+        return bBishops;
+    }
+
+    public long getBRooks() {
+        return bRooks;
+    }
+
+    public long getBQueens() {
+        return bQueens;
+    }
+
+    public long getBKing() {
+        return bKing;
+    }
+}
